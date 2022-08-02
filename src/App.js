499758c12075710eb9f8c5c4e6ffa6bc4a4144bf
Tlotliso Mokati <tlotlisomokati@gmail.com>
2022-08-02 14:33:11 +0200

    Added Interchanging button text

--- src/App.js
+++ src/App.js
@@ -6,22 +6,27 @@ import Topbread from "./Components/Topbread";
 import Lattice from "./Components/Lattice";
 import { useState } from "react";
 function App() {
-
   const [isShownTomato, setIsShownTomtato] = useState(true);
   const [isShownMeat, setIsShownMeat] = useState(true);
   const [isShownLatttice, setIsShownLattice] = useState(true);
+  const [isAddMeat, setIsAddMeat] = useState(false);
+  const [isAddTomato, setIsAddTomato] = useState(false);
+  const [isAddLattice, setIsAddLattice] = useState(false);
 
   const handleClickTomato = (event) => {
     setIsShownTomtato((current) => !current);
+    setIsAddTomato((current) => !current);
   };
 
   const handleClickLattice = (event) => {
     setIsShownLattice((current) => !current);
+    setIsAddLattice((current) => !current);
   };
 
-   const handleClickMeat = (event) => {
-     setIsShownMeat((current) => !current);
-   };
+  const handleClickMeat = (event) => {
+    setIsShownMeat((current) => !current);
+    setIsAddMeat((current) => !current);
+  };
 
   const tomato = "Tomato";
   const meat = "Meat";
@@ -30,45 +35,27 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <>ACA BURGER</>
+
         <Topbread />
-        {
-          isShownTomato && 
-          <Tomato ingredient={tomato} />
-        }
-        {
-          isShownMeat && 
-          <Meat ingredient={meat} />
-        }
-        {
-          isShownLatttice &&
-           <Lattice ingredient={lattice} />
-        }
+        {isShownTomato && <Tomato ingredient={tomato} />}
+        {isShownMeat && <Meat ingredient={meat} />}
+        {isShownLatttice && <Lattice ingredient={lattice} />}
         <BaseBread />
 
-
         <div className="button-group">
-          <button 
-            onClick={handleClickTomato} 
-            className="tomato-action"
-          >
-            Remove Tomato
+          <button onClick={handleClickTomato} className="tomato-action">
+            {isAddTomato ? "Add" : "Remove"} Tomato
           </button>
 
-          <button 
-            onClick={handleClickMeat} 
-            className="tomato-action"
-          >
-            Remove Meat
+          <button onClick={handleClickMeat} className="tomato-action">
+            {isAddMeat ? "Add" : "Remove"} Meat
           </button>
 
-          <button 
-            onClick={handleClickLattice}
-            className="tomato-action"
-          >
-            Remove Lattice
+          <button onClick={handleClickLattice} className="tomato-action">
+            {isAddLattice ? "Add" : "Remove"} Lattice
           </button>
         </div>
-        
       </header>
     </div>
   );
